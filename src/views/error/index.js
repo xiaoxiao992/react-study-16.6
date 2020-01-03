754fa62b015be2b854106e95a447dc15492ab9b4
xiao <15088777366@163.com>
2020-01-03 10:29:45 +0800

    错误页面pre标签问题

--- src/views/error/index.js
+++ src/views/error/index.js
@@ -4,20 +4,21 @@ const Index = () => {
     return (
         <div>
             <h1 align='center'>汪汪！你想找的页面出错了！</h1>
+            {/* pre标签换行需要自行加上{'\n'} 如果正常的html中无需如此 */}
             <pre align="center">
-                .----.
-            _.'__    `.
-            .---&copy;&nbsp;&nbsp;&copy;---/&hearts;\
-            &euro;          /&ensp;&hearts;&hearts;\
-            :        ,  &hearts;&hearts;&hearts;&hearts;
-            `_..__.-'_.-\&hearts;&hearts;/
-            `;_:    `"'
-            &gt;.&quot;&quot;&quot;&quot;&lt;
-            /,  hi , \\
-            //  你好！   \\
-            `-.________.-'
-            ___`. | .'____
-            (______|________)
+                .----.{'\n'}
+            _.'__    `.{'\n'}
+            .---&copy;&nbsp;&nbsp;&copy;---/&hearts;\{'\n'}
+            &euro;          /&ensp;&hearts;&hearts;\{'\n'}
+            :        ,  &hearts;&hearts;&hearts;&hearts;{'\n'}
+            `_..__.-'_.-\&hearts;&hearts;/{'\n'}
+            `;_:    `"'{'\n'}
+            &gt;.&quot;&quot;&quot;&quot;&lt;{'\n'}
+            /,  hi , \\{'\n'}
+            //  你好！   \\{'\n'}
+            `-.________.-'{'\n'}
+            ___`. | .'____{'\n'}
+            (______|________){'\n'}
             </pre>
         </div>
     )
